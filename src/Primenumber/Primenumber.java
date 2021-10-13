3f5ef885568210cf2082254cda6d7372f84d7678
akshay <kolge9@gmail.com>
2021-10-13 16:37:46 +0530

    fisrt init

---
+++ src/Primenumber/Primenumber.java
@@ -0,0 +1,15 @@
+package Primenumber;
+
+public class Primenumber {
+public static void main(String[] args) {
+	int num = 16;
+	int rem = num%2;
+	if(rem==0) {
+		System.out.println("Number is even");
+	}
+	else {
+		System.out.println("Number is odd");
+	}
+			
+}
+}
